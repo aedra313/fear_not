9fd24b90fca196d5b4ca772da1f5a90b84cc6238
Vlad <aedra313@gmail.com>
2023-02-20 17:49:30 +0200

    feature: labels

--- src/components/cardiogram/cardiogram.jsx
+++ src/components/cardiogram/cardiogram.jsx
@@ -1,18 +1,54 @@
 import React from 'react';
 import TICK_VALUES from './tickValues';
-import Label from '../misc/cardiogramMisc/label';
 import COORDINATES from './coordinates';
 import CONDITIONS from './conditions';
+import {VictoryAxis, VictoryChart, VictoryLine, VictoryScatter, VictoryTheme} from 'victory';
+import TOTAL from './total';
+import s from './cardiogram.module.css';
+import useWidth from '../../hooks/useWidth';
+import Labels from './labels/labels';
 // import DATA from './data';
 const Cardiogram = () => {
+  const WIDTH = useWidth();
   console.log(TICK_VALUES);
   console.log(COORDINATES);
   console.log(CONDITIONS);
+  console.log(TOTAL);
   // console.log(DATA.items.length());
   // console.log(DATA.items[2].day);
+  const totalWidth = TOTAL * 50 + 2*TOTAL;
+  const container = {
+    'width': WIDTH - 50 + 'px',
+  };
+  const wrap = {
+    'width': totalWidth + 'px',
+  };
   return (
     <div>
-      <Label color='#666' text="lorem" />
+      <div className={s.container} style={container}>
+        <div className={s.wrap} style={wrap}>
+          <VictoryChart
+            width={totalWidth}
+            theme={VictoryTheme.material}>
+            <VictoryAxis crossAxis
+              tickValues={TICK_VALUES}
+              orientation="top"
+              standalone={false} />
+            <VictoryAxis dependentAxis />
+
+            <VictoryLine
+              domain={{x: [0, 30], y: [0, 7]}}
+              style={{
+                data: {stroke: '#c43a31'},
+                parent: {border: '1px solid #ccc'},
+              }}
+              data={COORDINATES}
+            />
+            <VictoryScatter size={3} data = {COORDINATES} style={{data: {fill: 'red'}}}/>
+          </VictoryChart>
+          <Labels />
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/cardiogram/total.js
@@ -0,0 +1,5 @@
+import DATA from './data';
+
+const TOTAL = DATA.total;
+
+export default TOTAL;
