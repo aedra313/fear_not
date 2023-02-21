a410b8d3063b88ab3ca2fb8be091653c73ab9767
Vlad <aedra313@gmail.com>
2023-02-21 20:04:15 +0200

    fix cardiogram

--- src/components/cardiogram/cardiogram.jsx
+++ src/components/cardiogram/cardiogram.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import TICK_VALUES from './tickValues';
 import COORDINATES from './coordinates';
 import CONDITIONS from './conditions';
-import {VictoryAxis, VictoryChart, VictoryLine, VictoryScatter, VictoryTheme} from 'victory';
+import {LineSegment, VictoryAxis, VictoryChart, VictoryLine, VictoryScatter} from 'victory';
+import Theme from './theme';
 import TOTAL from './total';
 import s from './cardiogram.module.css';
 import useWidth from '../../hooks/useWidth';
@@ -16,28 +17,39 @@ const Cardiogram = () => {
   console.log(TOTAL);
   // console.log(DATA.items.length());
   // console.log(DATA.items[2].day);
-  const totalWidth = TOTAL * 50 + 2*TOTAL;
+  const totalWidth = TOTAL * 54 + TOTAL;
   const container = {
     'width': WIDTH - 50 + 'px',
   };
   const wrap = {
     'width': totalWidth + 'px',
   };
+  const strokeDasharray = '1, 48.3';
   return (
     <div>
       <div className={s.container} style={container}>
         <div className={s.wrap} style={wrap}>
           <VictoryChart
             width={totalWidth}
-            theme={VictoryTheme.material}>
+            theme={Theme}>
             <VictoryAxis crossAxis
               tickValues={TICK_VALUES}
               orientation="top"
+
               standalone={false} />
-            <VictoryAxis dependentAxis />
+            <VictoryAxis dependentAxis
+              offsetX={50}
+              gridComponent={<LineSegment x1='-25'/>}
+              style={{
+                grid: {
+                  strokeDasharray,
+                },
+              }
+              }/>
 
             <VictoryLine
               domain={{x: [0, 30], y: [0, 7]}}
+              padding={{left: 70}}
               style={{
                 data: {stroke: '#c43a31'},
                 parent: {border: '1px solid #ccc'},
